d64045f4691c13b99ab2f56bb08822dda2cc0be0
yeakiniqra <21201128@uap-bd.edu>
2024-06-22 20:59:28 +0600

    Some changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ function HeroSection() {
       />
         <div className="p-4 relative z-10 w-full text-center" >
             <h1
-            className="mt-10 md:mt-0 text-3xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
+            className="mt-16 md:mt-0 text-3xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
             >
                 Learn Coding & Development Online
             </h1>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,37 @@
 'use client';
 
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
-import Link from "next/link";
 
 export default function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
 
-  return (
-    <div
-    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-    >
-         <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Home">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/about">About</HoveredLink>
-            <HoveredLink href="/contact">Contact</HoveredLink>
-          </div>
-        </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Our Courses">
-          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
-          <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Android Development</HoveredLink>
-            <HoveredLink href="/branding">SEO Optimization</HoveredLink>
-          </div>
-        </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Pricing">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Freemium</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-          </div>
-        </MenuItem>
-
-        </Menu>
-      
-    </div>
-  )
+    return (
+        <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+            <Menu setActive={setActive}>
+                <MenuItem setActive={setActive} active={active} item="Home">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/about">About</HoveredLink>
+                        <HoveredLink href="/contact">Contact</HoveredLink>
+                    </div>
+                </MenuItem>
+                <MenuItem setActive={setActive} active={active} item="Our Courses">
+                    <div className="text-sm grid grid-cols-2 gap-10 p-4">
+                        <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                        <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+                        <HoveredLink href="/android-dev">Android Development</HoveredLink>
+                        <HoveredLink href="/seo">SEO Optimization</HoveredLink>
+                    </div>
+                </MenuItem>
+                <MenuItem setActive={setActive} active={active} item="Pricing">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/freemium">Freemium</HoveredLink>
+                        <HoveredLink href="/individual">Individual</HoveredLink>
+                        <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+                    </div>
+                </MenuItem>
+            </Menu>
+        </div>
+    )
 }
